86c4c1ae9cdca3e16518169fd447fb4bcbc39d14
Mariotd <mariotd1992@gmail.com>
2021-01-09 01:30:06 +0100

    :art: Better colors UI

--- main.cpp
+++ main.cpp
@@ -1,22 +1,27 @@
 #include "mainwindow.h"
-
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    qApp->setStyleSheet("MainWindow { background-color: qlineargradient(spread:pad, x1:1, y1:1, x2:0, y2:0, "
-                        "stop:0 #9b63f8, stop:1 #809ffc) }"
-                        "QGraphicsView { background-color: #c1c3f3 }"
+    qApp->setStyleSheet("MainWindow {"
+                        "background-color: qlineargradient(spread:pad, x1:1, y1:1, x2:0, y2:0,"
+                        "stop:0 #9b63f8, stop:1 #809ffc)"
+                        "}"
+                        "QGraphicsView { "
+                        "background-color: qlineargradient(spread:pad, x1:1, y1:1, x2:0, y2:0,"
+                        "stop:0 #989CE2, stop:1 #c1c3f3);"
+                        "border: 0px;"
+                        "}"
                         "QPushButton {"
                         "border-style: solid;"
                         "border-width: 2px;"
                         "border-radius: 10px;"
-                        "border-color: #f97da8;"
-                        "background-color: #f97da8;"
+                        "border-color: rgba(0, 0, 0, 0);"
+                        "background-color: qlineargradient(spread:pad, x1:1, y1:0, x2:0, y2:0,"
+                        "stop:0 #F4578E, stop:1 #EC3676);"
                         "color: white;"
                         "min-width: 6em;"
-                        "box-shadow: 10px 10px 8px 10px black;"
                         "}"
                         "QLabel {"
                         "color: white"
